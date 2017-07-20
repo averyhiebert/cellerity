59e8fa5d328def622f42d98bcc15a38056cbdadf
Avery Hiebert <averyhiebert@gmail.com>
2017-07-19 23:57:21 -0700

    Add several unit tests and fix bugs uncovered by said tests

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 /**
  * A module exposing a variety of functionality related to
  * simulating general-purpose cellular automata.
+ * Currently oncly includes the Automaton class, which allows the
+ * creation of general-purpose cellular automata.
  * @module cellulite
  */
-
-
 var Automaton = require("./automaton.js");
-/** The Automaton class */
 exports.Automaton = Automaton;
--- test/test.js
+++ test/test.js
@@ -4,13 +4,26 @@ var assert = require("assert");
 var Automaton = require("../src/main.js").Automaton;
 
 describe("Automaton", function(){
+    // Rule to use for testing (Conway's Game of Life)
+    function lifeUpdateRule(n){
+        //Note: n = neighbourhood, read by rows, as a linear array
+        //Assumes cells are either 1 or 0
+        var sum = n.reduce((x,y) => x + y,0);
+        if(n[4]){
+            var survive = (sum - 1) == 2 || (sum - 1) == 3;
+            return (survive?1:0);
+        }else{
+            return (sum == 3)?1:0;
+        }
+    }
+
     describe("#constructor()",function(){
         it("should initialize an automaton to default conditions", function(){
             var aut = new Automaton(() => 0);
             assert.equal(aut.data.length,20);
             assert.equal(aut.data[0].length,20);
             assert.equal(aut.data[0][0],0);
-        });
+        });// default conditions
 
         it("should support alternate dimensions", function(){
             var aut = new Automaton(() => 0,{
@@ -19,7 +32,7 @@ describe("Automaton", function(){
             });
             assert.equal(aut.data.length,3);
             assert.equal(aut.data[0].length,5);
-        });
+        });// alternate dimensions
 
         it("should support an alternate initialization function",function(){
             var aut = new Automaton(() => 0,{
@@ -33,21 +46,124 @@ describe("Automaton", function(){
                     assert.deepEqual(aut.data[i][j],[i,j,"test"]);
                 }
             }//for each cell in data array
+        });// alternate initialization
+
+        it("should support initializing using a set array",function(){
+            var startArray = [[1,2,3],[4,5,6],[7,8,9]];
+            var aut = new Automaton(() => 0,{
+                startData:startArray
+            });
+            assert.deepEqual(aut.data,startArray);
         });
     });// describe constructor
 
     describe("#step()",function(){
         it("should support a simple update rule",function(){
             var aut = new Automaton(n => n[4] + 1);
-            assert.equal(aut.data[1][1],0);
+            var initial = aut.data[1][1];
             aut.step();
-            assert.equal(aut.data[1][1],1);
+            assert.equal(aut.data[1][1],initial + 1);
             aut.step();
-            assert.equal(aut.data[1][1],2);
+            assert.equal(aut.data[1][1],initial + 2);
+            aut.step(5);
+            assert.equal(aut.data[1][1],initial + 7);
+        });// simple rule
+
+        it("should successfully execute Conway's Game of Life", function(){
+            var glider = [[0,0,0,0,0,0,0],
+                          [0,0,0,0,0,0,0],
+                          [0,0,0,0,0,0,0],
+                          [0,0,0,0,0,0,0],
+                          [0,0,0,0,1,1,0],
+                          [0,0,0,0,1,0,1],
+                          [0,0,0,0,1,0,0]];
+            var glider2 = [[0,0,0,0,0,0,0],
+                          [0,0,0,0,0,0,0],
+                          [0,0,0,0,0,0,0],
+                          [0,0,0,1,1,0,0],
+                          [0,0,0,1,0,1,0],
+                          [0,0,0,1,0,0,0],
+                          [0,0,0,0,0,0,0]];
+            var aut = new Automaton(lifeUpdateRule,{
+                startData:glider
+            });
+            aut.step(4);
+            assert.deepEqual(aut.data,glider2);
+        });// More complex rule
+
+        it("should use the 'toroid' edge mode by default",function(){
+            var grid = [[1,0,1],
+                        [0,0,0],
+                        [0,0,1]]
+            var grid2 = [[1,1,1],
+                        [1,1,1],
+                        [1,1,1]]
+            var aut = new Automaton(lifeUpdateRule,{
+                startData:grid
+            });
             aut.step();
-            assert.equal(aut.data[1][1],3);
+            assert.deepEqual(aut.data,grid2);
+        });// 'toroid' setting
+
+        it("should respect the 'cylinder' edge mode",function(){
+            var grid = [[1,0,1],
+                        [0,0,0],
+                        [0,0,1]]
+            var grid2 = [[1,0,1],
+                        [1,1,1],
+                        [0,0,1]]
+            var aut = new Automaton(lifeUpdateRule,{
+                startData:grid,
+                edgeMode:"cylinder"
+            });
             aut.step();
-            assert.equal(aut.data[1][1],4);
-        });
+            assert.deepEqual(aut.data,grid2);
+        });// 'cylinder' setting
+
+        it("should respect the 'freeze' edge mode",function(){
+            var grid = [[1,0,1],
+                        [0,0,0],
+                        [0,0,1]]
+            var grid2 = [[1,0,1],
+                        [0,1,0],
+                        [0,0,1]]
+            var aut = new Automaton(lifeUpdateRule,{
+                startData:grid,
+                edgeMode:"freeze"
+            });
+            aut.step();
+            assert.deepEqual(aut.data,grid2);
+        });// 'cylinder' setting
     });// describe step
+
+    describe("#reset()",function(){
+        it("should reset automaton data to initial conditions",function(){
+            var grid = [[1,2,3],[4,5,6],[7,8,9]];
+            var aut = new Automaton((n) => n[4] + 1,{
+                startData: grid
+            });
+            aut.step(5);
+            assert.notDeepEqual(aut.data,grid);
+            aut.reset();
+            assert.deepEqual(aut.data,grid);
+        });// basic reset functionality
+    });// describe reset
+
+    describe("#prettyPrint()",function(){
+        it("should format the array into a readable string", function(){
+            var aut = new Automaton(() => 0,{
+                startData:[[1,2,3],[4,5,6],[7,8,9]]
+            });
+            assert.equal(aut.prettyPrint(),"123\n456\n789");
+        });// format array into string
+
+        it("should use a provided map function to display cells",function(){
+            var aut = new Automaton(() => 0,{
+                startData:[[1,2,3],[4,5,6],[7,8,9]]
+            });           
+
+            var s = aut.prettyPrint((cell) => "(" + cell + ")");
+            assert.equal(s,"(1)(2)(3)\n(4)(5)(6)\n(7)(8)(9)");
+        });
+    });// describe prettyPrint
 });// describe Automaton
